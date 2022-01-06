7199a6a2675722aaafbed7df30fc9a8a4cd77870
RSThilakarathne <58285386+RSThilakarathne@users.noreply.github.com>
2022-01-06 20:34:16 +0530

    Add files via upload

---
+++ admin/user/includes/change password.php
@@ -0,0 +1,137 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+  <!-- Bootstrap Core CSS -->
+  <link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+  <!-- Custom CSS -->
+  <link href="css/style.css" rel='stylesheet' type='text/css' />
+  <!-- Graph CSS -->
+  <link href="css/font-awesome.css" rel="stylesheet"> 
+  <!-- jQuery -->
+  <!-- lined-icons -->
+  <link rel="stylesheet" href="css/icon-font.min.css" type='text/css' />
+  <!-- //lined-icons -->
+  <!-- chart -->
+  <script src="js/Chart.js"></script>
+  <!-- //chart -->
+  <!--animate-->
+  <link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+  <script src="js/wow.min.js"></script>
+  <script>
+    new WOW().init();
+  </script>
+  <!--//end-animate-->
+    <!----webfonts--->
+    <link href='//fonts.googleapis.com/css?family=Cabin:400,400italic,500,500italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
+    <!---//webfonts---> 
+    <!-- Meters graphs -->
+    <script src="js/jquery-1.10.2.min.js"></script>
+    <!-- Placed js at the end of the document so the pages load faster -->
+
+  </head> 
+
+  <body class="sign-in-up">
+    <section>
+      <div id="page-wrapper" class="sign-in-wrapper">
+        <div class="graphs">
+          <div class="sign-in-form">
+            <div class="sign-in-form-top">
+              <p><span>Change Password</span> </p>
+            </div>
+            <div class="signin">
+              
+              <form action="" method="post">
+
+                <div class="log-input">
+                  <div class="log-input-left">
+                    <input type="text" name="email" placeholder="Enter your email address" required=""/>
+                  </div>
+
+                </div>
+                <div class="log-input">
+                  <div class="log-input-left">
+                    <input type="password" name="password" placeholder="Enter your password" required=""/>
+                  </div>
+
+                </div>
+
+                <div class="log-input">
+                  <div class="log-input-left">
+                    <input type="password" name="password" placeholder="Confirm your password" required=""/>
+                  </div>
+
+                </div>
+                
+                <input type="submit" value="Change" name="s1">
+     
+              </form>	
+              <!--  -->
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+    <script src="js/jquery.nicescroll.js"></script>
+    <script src="js/scripts.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+  </html>
+
+
+<!--<!DOCTYPE html>
+<html lang="en">
+        <head>
+                <meta charset="utf-8">
+                <meta name="viewpoint"
+                content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <title> ABC Courier Service</title>
+                <link rel="stylesheet" href="css/bootstrap.css">
+                <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+            </head>
+    <body>
+<div class="model-dialog">
+	<div class="col-sm-4 main-section">
+	<div class="model-content">
+	<div class="col-12 form-input">
+<form>
+
+<h2>Change Password</h2>
+	<div class="form-group">
+		<label for="Inputname"><h4><b>Name</b></h4></label>
+		<input type="text" class="form-control">
+	</div>
+
+	<div class="form-group">
+		<label for="Inputemail"><h4><b>Email Address</b></h4></label>
+		<input type="email" class="form-control">
+	</div>
+	<div class="form-group">
+		<label for="Inputpassword"><h4><b>New Password</b></h4></label>
+		<input type="password" class="form-control">
+	</div>
+
+	<div class="form-group">
+		<label for="newpassword"><h4><b>Confirm Password</b></h4></label>
+		<input type="password" class="form-control">
+	</div>
+
+	
+	<button type="submit" class="btn btn-success btn-block"><b>Submit</b></button>
+</form>
+	</div>
+	
+
+</div>
+</div>
+</div>
+</body>
+</html>-->
