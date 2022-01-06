39ff1fd3bf19b9dcadebb7f904463b30139dc9ed
RSThilakarathne <58285386+RSThilakarathne@users.noreply.github.com>
2022-01-06 19:43:43 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <title>Home| ABC Courier Service</title>
+	<meta charset="utf-8">
+	<meta name="viewpoint" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/stellarnav.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+	<link rel="stylesheet" href="css/bootstrap.css"> 
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link href="style1.css" rel="stylesheet" type="text/css" media="screen"/>
+  <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href=""><img src="Images/28.png" width="15%" height="15%"> Courier Service</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active"><a href="index.php" class="nav-link">Home<span class="sr-only"></span></a></li>
+                    <li class="nav-item"><a href="#about" class="nav-link">About Us</a></li>
+                    <li class="nav-item"><a href="#services" class="nav-link">Services</a></li>
+                    <li class="nav-item"><a href="#contact" class="nav-link">Contact Us</a></li>
+                    
+                    <!--<form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit" ><i class="fa fa-search"></i></button>
+                    </form>-->
+                    <li class="login" style="color: #fff;"><i class="fa fa-user"></i><a href="admin/register.php"> Sign Up</a> / <a href="admin/index.php">Sign In</a></li>
+                    </ul>
+                </div>
+            </nav>
+
+             <div class="carousel slide" data-ride="carousel" id="exampleController">
+                <ol class="carousel-indicators">
+                    <li data-target="#exampleController" data-slide-to="0" class="active"></li>
+                    <li data-target="#exampleController" data-slide-to="1"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active" data-interval="3000">
+                        <img src="images/carousel.jpg"  alt="">	
+                        <div class="carousel-caption"><h1 style="color: #fff;">FUELING YOUR<br> LOGISTICS CHAIN</h1>
+                        </div>
+                    </div>
+                    <div class="carousel-item" data-interval="3000">
+                        <img src="images/lorry.jpg" alt="">	
+                        <div class="carousel-caption"><h1 style="color: #fff;text-align: left;margin-left: 200px;">LOGISTICS THROUGH<br> SPEED AND SECURITY</h1></div>	
+                    </div>
+                </div>
+                    
+                    <a href="#exampleController" class="carousel-control-prev" data-slide="prev">
+                    <span class="carousel-control-prev-icon"></span>
+                    </a>
+                    <a href="#exampleController" class="carousel-control-next" data-slide="next">
+                        <span class="carousel-control-next-icon"></span>
+                    </a>
+                </div>
+
+                <div class="intro">
+                <div class="welcome">
+                    <h3><b>Welcome to the ABC courier service</b></h3>
+                </div>
+                    <div class="blog">
+                        <p>ABC courier service is the largest and most experienced domestic courier service company in the Island handling documents and packages for corporate and individual clients.
+                            Over the years its services and operational systems are tailor made which could cater to the varying needs of the customers.
+                            Speed, security, reliability and accountability are considered paramount in its services afforded to every customer.</p>
+                    </div>
+                </div>
+
+          <section id="about">
+            <div class="about">
+        <h3>ABOUT US</h3>
+      </div>
+      <div class="row">
+      <div class="col-md-5">
+        <div class="img">
+        <img src="Images/promo_top_bg.png" alt="" width="80%" height="50%">
+        </div>
+      </div>
+      <div class="col-md-7">
+          <div class="about-content-area wow fadeIn">
+            <div class="about-content">
+              <h2>We have 10 years experience in this passion</h2>
+              <p>One Decade of Experience with Speed Reliability and flexibility. With our logistics capabilities and expertise, we can be trusted to be the reliable partner to fuel your business growth.</p>
+            </div>
+          </div>
+      </div>
+    </div>
+           <div class="container">
+               <div class="card-deck">
+                <div class="card">
+                   <img src="images/slide_2.jpg" alt="" class="card-img-top">
+                   <div class="card-meta"><i class="fa fa-truck"></i></div>
+                   <div class="card-body">
+                       <h5 class="card-title"><b>Ontime Delivery</b></h5>
+                        <p class="card-text">You can be sure your urgent, valuable items will be delivered safely on time.</p>
+                   </div>
+                   <div class="card-footer">
+                    <a href="" class="read-more">Read More</a>
+                   </div>
+                </div>
+                <div class="card">
+                    <img src="images/tracking.jpeg" alt="" class="card-img-top">
+                    <div class="card-meta"><i class="fa fa-map-marker"></i></div>
+                    <div class="card-body">
+                        <h5 class="card-title"><b>Online Tracking</b></h5>
+                         <p class="card-text">ABC courier service offers user friendly online parcel / document tracking solution in Sri Lanka.</p>
+                    </div>
+                    <div class="card-footer">
+                     <a href="" class="read-more">Read More</a>
+                    </div>
+                 </div>
+                 <div class="card">
+                    <img src="images/32.jpeg" alt="" class="card-img-top">
+                    <div class="card-meta"><i class="fa fa-laptop"></i></div>
+                    <div class="card-body">
+                        <h5 class="card-title"><b>Online Payment</b></h5>
+                         <p class="card-text">You can easily pay the money via online using your credit card.</p>
+                    </div>
+                    <div class="card-footer">
+                     <a href="" class="read-more">Read More</a>
+                    </div>
+                 </div>
+               </div>
+           </div> 
+    </section>
+
+           <div class="services">
+           <section id="services">
+               <div class="container">
+                <h3>OUR SERVICES</h3>
+                <div class="row">
+                    <div class="col-md-3 text-center">
+                        <i class="fa fa-truck" alt=""></i><h4>Fast Delivery</h4></div>
+                    <div class="col-md-3 text-center">
+                        <i class="fa fa-phone" alt=""></i><h4>24/7 Service</h4></div>
+                    <div class="col-md-3 text-center">
+                        <i class="fa fa-thumbs-up" alt=""></i><h4>Corporate Services</h4></div>
+                    <div class="col-md-3 text-center">    
+                        <i class="fa fa-map-marker" alt=""></i><h4>Efficient Tracking System</h4>
+                    </div>
+                </div>
+            </div>
+           </section>
+           </div>
+
+    <div class="countertable">
+    <div class="row">
+        <div class="col-md-3"><img alt="" class="countertable_icon" src="Images/building_icon.png">
+            <span class="data-counter-up" data-min="0" data-max="20" data-delay="5" data-increment="5" data-slno="1">20+</span><h5>Islandwide Branches</h5></div>
+        <div class="col-md-3"><img alt="" class="countertable_icon" src="Images/customers_icon.png">
+            <span class="data-counter-up" data-min="0" data-max="5000" data-delay="5" data-increment="80" data-slno="2">2000+</span><h5>Customers Islandwide</h5></div>
+        <div class="col-md-3"><img alt="" class="countertable_icon" src="Images/delivery_truck_icon.png">
+            <span class="data-counter-up" data-min="0" data-max="200" data-delay="5" data-increment="5" data-slno="3">50+</span><h5>Owned Vehicles</h5></div>
+        <div class="col-md-3"><img alt="" class="countertable_icon" src="Images/employee_icon.png">
+            <span class="data-counter-up" data-min="0" data-max="300" data-delay="5" data-increment="3" data-slno="4">100+</span><h5>Employees</h5></div>
+    </div>
+    </div>
+             
+
+        <footer class="site-footer">
+          <section id="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="row">
+            <div class="col-md-4">
+            <h2 class="footer-heading mb-4">Send Us Message</h2>
+            <form action="#" method="post">
+              <div class="input-group mb-3">
+                <input type="text" class="form-control border-secondary text-white bg-transparent" placeholder="Enter Name" aria-label="Enter Name" aria-describedby="button-addon2">
+              </div>
+              <div class="input-group mb-3">
+                <input type="text" class="form-control border-secondary text-white bg-transparent" placeholder="Enter Email" aria-label="Enter Email" aria-describedby="button-addon2">
+              </div>
+              <div class="input-group mb-3">
+                <input type="textarea" class="form-control border-secondary text-white bg-transparent" placeholder="Enter Message" aria-label="Enter Message" aria-describedby="button-addon2">
+              </div>
+                <div class="input-group-append">
+                  <button class="btn btn-primary text-white" type="button" id="button-addon2">Send</button>
+                </div>
+              
+            </form>
+          </div>
+              <div class="col-md-3 ml-auto">
+                <h2 class="footer-heading mb-4">Features</h2>
+                <ul class="list-unstyled">
+                  <li><a href="index.php">Home</a></li>
+                  <li><a href="#about">About Us</a></li>
+                  <li><a href="#services">Services</a></li>
+                  <li><a href="#contact">Contact Us</a></li>
+                </ul>
+              </div>
+              <div class="col-md-4">
+                <h2 class="footer-heading mb-4">Follow Us</h2>
+                <a href="#" class="pl-0 pr-3"><span class="fa fa-facebook"></span></a>
+                <a href="#" class="pl-3 pr-3"><span class="fa fa-twitter"></span></a>
+                <a href="#" class="pl-3 pr-3"><span class="fa fa-instagram"></span></a>
+                <a href="#" class="pl-3 pr-3"><span class="fa fa-linkedin"></span></a>
+              </div>
+            </div>
+          </div>
+ 
+          </div>
+        </div>
+       
+
+          <a href="#" class="scrollup"><i class="fa fa-angle-up fa-square fa-bglight fa-2x active"></i></a>
+       
+      </div>
+      </section>
+    </footer>
+                        
+     
+        <script src="js/jquery.js"></script>
+		<script src="js/bootstrap.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="Dashboard/js/counterup/waypoints.min.js"></script>
+        <script src="Dashboard/js/counterup/jquery.counterup.min.js"></script>
+        
+        <script> jQuery(document).ready(function(){
+            $('.counter').counterUp({
+            delay: 10,
+            time: 1000
+        });
+        });
+        </script>
+</body>
+</html>
