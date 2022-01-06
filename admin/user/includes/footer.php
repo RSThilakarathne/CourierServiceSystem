658b5a4257db80ff6a6e04d7f40f331f0677f9fb
RSThilakarathne <58285386+RSThilakarathne@users.noreply.github.com>
2022-01-06 20:16:28 +0530

    Add files via upload

---
+++ admin/user/includes/footer.php
@@ -0,0 +1,8 @@
+  
+  
+  <footer class="bg-inverse">
+        <div class="container">
+            <p class="m-0 text-center text-white"> Courier Service System </p>
+        </div>
+        <!-- /.container -->
+    </footer>
