a8fdfc3666ce0aca143dbca9ef5cd97951b7a0d6
RSThilakarathne <58285386+RSThilakarathne@users.noreply.github.com>
2022-01-06 20:25:19 +0530

    Delete admin/user directory

--- admin/user/includes/footer.php
+++
@@ -1,8 +0,0 @@
-  
-  
-  <footer class="bg-inverse">
-        <div class="container">
-            <p class="m-0 text-center text-white"> Courier Service System </p>
-        </div>
-        <!-- /.container -->
-    </footer>
